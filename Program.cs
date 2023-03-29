64d16434760ed5dbe0abcaae1849783a5fd691f6
dasha <dasha@gmail.ru>
2023-03-29 13:36:41 +0300

    popa

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Коэфицент a:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введит b:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите c:");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"D = b*2 - 4*a*c = {Math.Pow(b, 2) - 4 * a * c}");
+double d = Math.Pow(b, 2) - 4 * a * c;
+if (d < 0)
+{
+    Console.WriteLine("Корня нет");
+}
+else if (d == 0)
+{
+    Console.WriteLine($"x = {(-b) / (2 * a)}");
+}
+else if (d > 0)
+{
+    Console.WriteLine($"х-первое число = {(-b + Math.Sqrt(d)) / (2 * a)}");
+    Console.WriteLine($"х-второе число = {(-b - Math.Sqrt(d)) / (2 * a)}");
+}
+
+
